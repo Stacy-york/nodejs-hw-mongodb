739a2d946edbec20c3b384bd00a93149c7c0ed7d
Stacy York <stacyyyork@gmail.com>
2025-08-04 17:54:21 +0300

    add documentation for API using Swagger

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import contactsRouter from './routers/contacts.js';
 import authRouter from './routers/auth.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
 
@@ -23,6 +24,7 @@ export const setupServer = () => {
   app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(notFoundHandler);
   app.use(errorHandler);
